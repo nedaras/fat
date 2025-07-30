0bc63413bc6d297a59bedf65087d22c54f443484
Nedas Pranskunas <nedas.pranskunas@gmail.com>
2025-07-30 17:46:36 +0300

    Added public module of Library.zig

--- build.zig
+++ build.zig
@@ -25,8 +25,9 @@ pub fn build(b: *std.Build) void {
 
     options_mod.addOptions("build_options", options);
 
-    const lib_mod = b.createModule(.{
-        .root_source_file = b.path("src/lib_c.zig"),
+    // bad but will work it out
+    const lib_mod = b.addModule("fat", .{
+        .root_source_file = b.path("src/Library.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -55,33 +56,33 @@ pub fn build(b: *std.Build) void {
 
     b.installArtifact(lib);
 
-    const exe_mod = b.createModule(.{
-        .target = target,
-        .optimize = optimize,
-    });
+    //const exe_mod = b.createModule(.{
+        //.target = target,
+        //.optimize = optimize,
+    //});
 
-    const exe = b.addExecutable(.{
-        .name = "fat",
-        .root_module = exe_mod,
-    });
+    //const exe = b.addExecutable(.{
+        //.name = "fat",
+        //.root_module = exe_mod,
+    //});
 
-    exe.linkLibC();
-    exe.linkLibrary(lib);
+    //exe.linkLibC();
+    //exe.linkLibrary(lib);
 
-    exe.addCSourceFile(.{ .file = b.path("main.c") });
+    //exe.addCSourceFile(.{ .file = b.path("main.c") });
 
-    b.installArtifact(exe);
+    //b.installArtifact(exe);
 
-    const run_cmd = b.addRunArtifact(exe);
+    //const run_cmd = b.addRunArtifact(exe);
 
-    run_cmd.step.dependOn(b.getInstallStep());
+    //run_cmd.step.dependOn(b.getInstallStep());
 
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
+    //if (b.args) |args| {
+        //run_cmd.addArgs(args);
+    //}
 
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    //const run_step = b.step("run", "Run the app");
+    //run_step.dependOn(&run_cmd.step);
 
     const lib_unit_tests = b.addTest(.{
         .root_module = lib_mod,
